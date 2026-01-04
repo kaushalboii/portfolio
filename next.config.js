23b3bde387626516daf6ca67f03e8b90901c5aee
Kaushal <kaushal@kaushals-MacBook-Air.local>
2026-01-04 14:53:28 +0530

    Deploy static site to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
+  output: "export",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
